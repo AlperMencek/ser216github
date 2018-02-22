5c08972c92d668a8225244282511d085c0c3f702
AlperMencek <33849897+AlperMencek@users.noreply.github.com>
2018-02-22 14:35:38 -0700

    Add files via upload

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,8 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		if ("Hello World".equals("Hello World"))
-			System.out.println("Hello World");
-		else
-			System.out.println("What...");
+		System.out.println("Hello World change by Alper Mencek“);
+		
 	}
 
 }
